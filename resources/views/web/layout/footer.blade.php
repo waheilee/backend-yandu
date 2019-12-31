2cfb57548b8e22ec74a1b90349c89104f7e98c98
waheilee <waheilee@gmail.com>
2019-12-31 15:25:50 +0800

    增加备案链接

--- resources/views/web/layout/footer.blade.php
+++ resources/views/web/layout/footer.blade.php
@@ -3,6 +3,6 @@
         <ul class="bs-docs-footer-links">
             <li><a href="https://yd-hb.com/" target="_blank">严度官网</a></li>
         </ul>
-        <p>Copyright © 2019 严度测评 | 京ICP备19027114号-1</p>
+        <p>Copyright © 2019 严度服务 | <a href="http://www.beian.miit.gov.cn/" target="_blank">京ICP备19027114号-1</a></p>
     </div>
 </footer>
