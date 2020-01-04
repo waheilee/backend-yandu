20e00581f2614e9e908c8ceb8fa751ef427b5877
waheilee <waheilee@gmail.com>
2020-01-04 23:06:00 +0800

    微信授权登录

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -50,6 +50,10 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             'bindings',
         ],
+
+        'wap' => [
+            \Illuminate\Session\Middleware\StartSession::class,
+        ]
     ];
 
     /**
--- routes/wap.php
+++ routes/wap.php
@@ -2,8 +2,8 @@
 //微信服务接口
 Route::any('wechat', 'WeChatController@serve');
 
-Route::middleware(['wechat.oauth'])->group(function (){
+Route::group(['middleware' => ['auth:wap']],function (){
     //施工人员工牌信息与评价
-    Route::get('worker/info/index','WorkerInfoController@index');
+    Route::get('worker/info/index','WorkerInfoController@index')->middleware(['wechat.oauth']);
 });
 
