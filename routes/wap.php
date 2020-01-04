db0ca98a979e4d0a8ee0a3eaf50c2f0261dd6f7a
waheilee <waheilee@gmail.com>
2020-01-04 23:07:58 +0800

    微信授权登录

--- routes/wap.php
+++ routes/wap.php
@@ -2,7 +2,7 @@
 //微信服务接口
 Route::any('wechat', 'WeChatController@serve');
 
-Route::group(['middleware' => ['auth:wap']],function (){
+Route::group(['middleware' => ['wap']],function (){
     //施工人员工牌信息与评价
     Route::get('worker/info/index','WorkerInfoController@index')->middleware(['wechat.oauth']);
 });
