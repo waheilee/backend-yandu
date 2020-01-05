f10670e1c3be45d968ce18dca76bb661ca19b1bd
waheilee <waheilee@gmail.com>
2020-01-05 22:20:22 +0800

    获取微信用户信息测试

--- app/Http/Controllers/Wap/WorkerInfoController.php
+++ app/Http/Controllers/Wap/WorkerInfoController.php
@@ -5,25 +5,37 @@ namespace App\Http\Controllers\Wap;
 
 use App\Http\Controllers\Controller;
 use App\Models\Member;
+use App\Models\Worker;
 use EasyWeChat\Factory;
+use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 class WorkerInfoController extends Controller
 {
-    /**
-     * @return Member|\Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Model|null|object
-     */
-    public function index()
+
+    protected $user;
+
+    public function __construct()
     {
-        $user = session('wechat.oauth_user.default'); //一句话， 拿到授权用户资料
-        $openid = $user->getOriginal()['openid'];
-        $web_openid = $user->getId();
-        $member = Member::where('openid', $user->getId())->first();
+        $this->user = session('wechat.oauth_user.default');//一句话， 拿到授权用户资料
+    }
+
+    /**
+     * @param Request $request
+     * @return Member|\Illuminate\Contracts\View\Factory|\Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Model|\Illuminate\View\View|null|object
+     */
+    public function index(Request $request)
+    {
+
+        $openid = $this->user->getOriginal()['openid'];
+        $web_openid = $this->user->getId();
+        $member = Member::where('openid', $this->user->getId())->first();
+        $worker = Worker::whereId($request->input('worker_id'))->first();
         //校验用户是否存在，不存在则把信息保存在数据库
         if (!$member) {
-            $userModel = Member::create([
-                'nickname' => $user->getNickname(),
-                'avatar' => $user->getAvatar(),
+            $member = Member::create([
+                'nickname' => $this->user->getNickname(),
+                'avatar' => $this->user->getAvatar(),
                 'openid' => $openid,
                 'web_openid' => $web_openid,
                 'api_token' => hash('sha256', Str::random(60)),
@@ -32,14 +44,11 @@ class WorkerInfoController extends Controller
                 'policy' => 0,
                 'mini_openid' => '-'
             ]);
-            dd($userModel);
-            return $userModel;
-        }else{
-            dd($member);
+//            dd($userModel);
             return $member;
         }
 
 //        dd($member);
-//        return view('wap.worker_info.index');
+        return view('wap.worker_info.index',compact('worker','member'));
     }
 }
