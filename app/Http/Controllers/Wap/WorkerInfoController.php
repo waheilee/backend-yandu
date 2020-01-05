5479103157d1aed767fa4ef9ecb902f6cd8263c9
waheilee <waheilee@gmail.com>
2020-01-05 21:28:25 +0800

    获取微信用户信息测试

--- app/Http/Controllers/Wap/WorkerInfoController.php
+++ app/Http/Controllers/Wap/WorkerInfoController.php
@@ -10,12 +10,9 @@ class WorkerInfoController extends Controller
 
     public function index()
     {
-        $config = config('wechat.official_account');
-        $app = Factory::officialAccount($config);
-        $oauth = $app->oauth;
-        dd($oauth);
 //        $user = session('wechat.oauth_user.default'); //一句话， 拿到授权用户资料
-//        dd($user);
+        $user = $_SESSION['wechat_user'];
+        dd($user);
         return view('wap.worker_info.index');
     }
 }
