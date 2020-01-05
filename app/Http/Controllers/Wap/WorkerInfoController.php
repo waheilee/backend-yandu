e25c68c43eed42c3be3dcdb547f1fc40e71c1445
waheilee <waheilee@gmail.com>
2020-01-05 21:57:38 +0800

    获取微信用户信息测试

--- app/Http/Controllers/Wap/WorkerInfoController.php
+++ app/Http/Controllers/Wap/WorkerInfoController.php
@@ -6,17 +6,37 @@ namespace App\Http\Controllers\Wap;
 use App\Http\Controllers\Controller;
 use App\Models\Member;
 use EasyWeChat\Factory;
+use Illuminate\Support\Str;
+
 class WorkerInfoController extends Controller
 {
     /**
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @return Member|\Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Model|null|object
      */
     public function index()
     {
         $user = session('wechat.oauth_user.default'); //一句话， 拿到授权用户资料
-        dd($user);
+        $openid = $user->getOriginal()['openid'];
+        $web_openid = $user->getId();
         $member = Member::where('openid', $user->getId())->first();
-        dd($member);
-        return view('wap.worker_info.index');
+        //校验用户是否存在，不存在则把信息保存在数据库
+        if (!$user) {
+            $userModel = Member::create([
+                'nickname' => $user->getNickname(),
+                'avatar' => $user->getAvatar(),
+                'openid' => $openid,
+                'web_openid' => $web_openid,
+                'api_token' => hash('sha256', Str::random(60)),
+                'deposit' => 0,
+                'rent' => 0,
+                'policy' => 0
+            ]);
+            return $userModel;
+        }else{
+            return $member;
+        }
+
+//        dd($member);
+//        return view('wap.worker_info.index');
     }
 }
