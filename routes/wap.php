ebd0f39094f9a545105878d758da07a22ccef9a5
waheilee <waheilee@gmail.com>
2020-01-06 19:21:54 +0800

    工人工牌信息页

--- routes/wap.php
+++ routes/wap.php
@@ -5,8 +5,8 @@ Route::any('wechat', 'WeChatController@serve');
 Route::group(['middleware' => ['wap']],function (){
     //施工人员工牌信息与评价
     Route::get('worker/info/index','WorkerInfoController@index')->middleware(['wechat.oauth']);
-    Route::get('worker/info/evaluate/{id}','WorkerInfoController@evaluate')->middleware(['wechat.oauth']);
-    Route::post('worker/info/store/evaluate','WorkerInfoController@storeEvaluate')->name('storeEvaluate')->middleware(['wechat.oauth']);
+    Route::get('worker/info/evaluate/{id}','WorkerInfoController@evaluate');
+    Route::post('worker/info/store/evaluate','WorkerInfoController@storeEvaluate')->name('storeEvaluate');
 
 });
 
